5cf3d416685ad1e9c510d9dda111f9e0f8b683b0
Shreya <shreyaray.1206@gmail.com>
2024-05-20 22:39:26 +0530

    prac inception

---
+++ prac/App.js
@@ -0,0 +1,5 @@
+const heading  = React.createElement('h1', {id : "heading"}, "Hello from react🚀");
+console.log(heading)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(root);
+root.render(heading);
