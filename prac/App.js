03780f7b16c0bd1917df92f74408a871a9697a30
Shreya <shreyaray.1206@gmail.com>
2024-05-21 01:43:58 +0530

    prac inception

--- prac/App.js
+++ prac/App.js
@@ -1,5 +1,19 @@
-const heading  = React.createElement('h1', {id : "heading"}, "Hello from react🚀");
-console.log(heading)
+// make this in React.createElement
+{/* <div>
+  <div className="child">
+    <h1></h1>
+  </div>
+</div> */}  
+
+const parent = React.createElement('div', {id:"parent"}, 
+               React.createElement('div', {id:"child"}, 
+               React.createElement('h1', {id:"heading"}, "Hello From React Element 😁")))
+
+console.log(parent);
+
+
+// const heading  = React.createElement('h1', {id : "heading"}, "Hello from react🚀");
+// console.log(heading)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 console.log(root);
-root.render(heading);
+root.render(parent);
