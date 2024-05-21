fb942d39634715e247d8c877b7287d0d41af4cda
Shreya <shreyaray.1206@gmail.com>
2024-05-21 12:09:09 +0530

    hello-react-prac

--- prac/App.js
+++ prac/App.js
@@ -11,9 +11,15 @@ const parent = React.createElement('div', {id:"parent"},
 
 console.log(parent);
 
+const grandParent =  [React.createElement('div', {id:"parent"}, 
+React.createElement('div', {id:"child"}, [React.createElement('h1', {id:"heading"}, "Hello From React Element 😁"),
+React.createElement('h2', {id:"heading"}, "Hello 2 From React Element 😁")])),
+React.createElement('div', {id:"parent"}, 
+React.createElement('div', {id:"child"}, [React.createElement('h1', {id:"heading"}, "Hello From React Element 😁"),
+React.createElement('h2', {id:"heading"}, "Hello 2 From React Element 😁")]))]
 
 // const heading  = React.createElement('h1', {id : "heading"}, "Hello from react🚀");
 // console.log(heading)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 console.log(root);
-root.render(parent);
+root.render(grandParent);
